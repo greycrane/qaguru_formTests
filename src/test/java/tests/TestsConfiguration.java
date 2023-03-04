6da639c13a2896c47da4ebc5e9b6476cb40dcf6b
szhuravlev <gcrane@bk.ru>
2023-03-04 15:16:06 +0300

    selenoid address fix

--- src/test/java/tests/TestsConfiguration.java
+++ src/test/java/tests/TestsConfiguration.java
@@ -8,15 +8,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static io.qameta.allure.internal.shadowed.jackson.databind.type.LogicalType.Map;
-
 public class TestsConfiguration {
     public void configureTests() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
         Configuration.browserVersion = "100.0";
-        Configuration.remote = "https://user1:1234@selemoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
@@ -30,7 +28,7 @@ public class TestsConfiguration {
 
     @AfterEach
     void addAttachments() {
-        Attachments.screenshotAs("Last screenshot");
+        Attachments.screenshotAs("Final screenshot");
         Attachments.pageSource();
         Attachments.browserConsoleLogs();
         Attachments.addVideo();
